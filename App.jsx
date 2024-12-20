85b1c198b0030ce9032318d416df56e78f4cfaf9
ozerbaykal <baykalozer87@gmail.com>
2024-12-20 16:56:53 +0300

    docs:coponent,screens,navigation,context folder add to src and feat:router.js initialized

---
+++ App.jsx
@@ -0,0 +1,8 @@
+import {SafeAreaView} from 'react-native';
+import Routes from './src/navigation/Routes';
+
+const App = () => {
+  return <Routes />;
+};
+
+export default App;
---
+++ src/navigation/Routes.js
@@ -0,0 +1,18 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from '../screens/LoginScreen';
+import HomeScreen from '../screens/HomeScreen';
+
+const Routes = () => {
+    const Stack = createNativeStackNavigator();
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="Home" component={HomeScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default Routes;
